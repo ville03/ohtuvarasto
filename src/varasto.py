814e307d487dbcd3181ab09a98774da0006b73de
ville03 <ville.r.p.parkkinen@gmail.com>
2024-11-04 22:30:09 +0200

    fix

--- src/varasto.py
+++ src/varasto.py
@@ -25,7 +25,7 @@ class Varasto:
         if maara < 0:
             return
         if maara <= self.paljonko_mahtuu():
-            self.saldo = self.saldo + maara+4
+            self.saldo = self.saldo + maara
         else:
             self.saldo = self.tilavuus
 
